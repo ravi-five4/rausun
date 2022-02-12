678b3dca016238531f987039c84fe8f23f989227
ravi.gandhi <ravi.gandhi@justdial.com>
2022-02-12 16:12:31 +0530

    testing changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hi , I Am Raushan Gandhi</h1>
+        <h1>Hi , I Am Ravi Gandhi</h1>
         <p>This is my First Testing website by using React Js</p>
       </header>
     </div>
