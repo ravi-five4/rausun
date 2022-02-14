014cd5e854ee679452b180aea2c30706de65a806
ravi.gandhi <ravi.gandhi@justdial.com>
2022-02-14 20:01:14 +0530

    testing

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,6 +6,7 @@ const Footer = () => {
       <div className="container-fluid nav_bg">
         <div className="row">
           <div className="col-10 mx-auto">
+            <h1>Ravi Gandhi</h1>
             <span>ravi.five4@gmail.com</span>
             <p>+91 9110132757</p>
           </div>
